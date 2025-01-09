1bf857becf4bddf2a40a09d3627b87890a9bdb2e
devcastlecix <luis_castle_2011@hotmai.com>
2025-01-09 18:50:01 -0500

    enhanced FractionTest class #7

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -1,6 +1,7 @@
 package es.upm.miw.iwvg_devops.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,26 +10,31 @@ import org.junit.jupiter.api.Test;
 class FractionTest {
     private Fraction fractionOne;
     private Fraction fractionError;
+    private Fraction twoThird;
+    private Fraction fractionHalf;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         fractionOne = new Fraction();
         fractionError = new Fraction(0,0);
+        fractionHalf = new Fraction(1,2);
+        twoThird = new Fraction(2,3);
     }
+
     @Test
-    void testFractionDefaultConstructor(){
+    void testFractionDefaultConstructor() {
         assertEquals(1,fractionOne.getNumerator());
         assertEquals(1,fractionOne.getDenominator());
     }
 
     @Test
-    void testFractionGetNumerator(){
+    void testFractionGetNumerator() {
         assertEquals(1,fractionOne.getNumerator());
         assertEquals(0,fractionError.getNumerator());
     }
 
     @Test
-    void testFractionSetNumerator(){
+    void testFractionSetNumerator() {
         int newNumerator = 2;
         fractionOne.setNumerator(newNumerator);
         fractionError.setNumerator(newNumerator);
@@ -37,13 +43,13 @@ class FractionTest {
     }
 
     @Test
-    void testFractionGetDenominator(){
+    void testFractionGetDenominator() {
         assertEquals(1,fractionOne.getDenominator());
         assertEquals(0,fractionError.getDenominator());
     }
 
     @Test
-    void testFractionSetDenominator(){
+    void testFractionSetDenominator() {
         int newDenominator = 2;
         fractionOne.setDenominator(newDenominator);
         fractionError.setDenominator(newDenominator);
@@ -52,7 +58,7 @@ class FractionTest {
     }
 
     @Test
-    void testFractionToString(){
+    void testFractionToString() {
         String fractionOneString = "Fraction{" +
                 "numerator=" + '1' +
                 ", denominator=" + '1' +
@@ -62,20 +68,79 @@ class FractionTest {
                 ", denominator=" + '0' +
                 '}';
         assertEquals(fractionOneString,fractionOne.toString());
+        assertEquals(fractionErrorString,fractionError.toString());
     }
 
     @Test
-    void testFractionDecimal(){
+    void testFractionDecimal() {
         assertEquals(1,fractionOne.decimal());
         fractionOne.setDenominator(2);
         assertEquals(0.5,fractionOne.decimal());
     }
 
     @Test
-    void testErrorFractionDecimal(){
+    void testErrorFractionDecimal() {
         assertTrue(Double.isNaN(fractionError.decimal()));
         fractionError.setDenominator(1);
         assertEquals(0,fractionError.decimal());
     }
 
+    @Test
+    void testIsProper() {
+        assertFalse(fractionOne.isProper());
+        assertFalse(fractionError.isProper());
+        assertTrue(fractionHalf.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertTrue(fractionOne.isImproper());
+        assertTrue(fractionError.isImproper());
+        assertFalse(fractionHalf.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction otherFraction = new Fraction(2,2);
+        Fraction otherFractionOne = new Fraction();
+        assertFalse(fractionOne.isEquivalent(fractionHalf));
+        assertTrue(fractionOne.isEquivalent(otherFractionOne));
+        assertTrue(fractionOne.isEquivalent(otherFraction));
+    }
+
+    @Test
+    void testAddFractions() {
+        Fraction otherFractionTwoThird = new Fraction(2,3);
+        otherFractionTwoThird.add(fractionHalf);
+        fractionHalf.add(twoThird);
+        fractionOne.add(twoThird);
+        assertEquals(7,fractionHalf.getNumerator());
+        assertEquals(6,fractionHalf.getDenominator());
+        assertEquals(7,otherFractionTwoThird.getNumerator());
+        assertEquals(6,otherFractionTwoThird.getDenominator());
+        assertEquals(5,fractionOne.getNumerator());
+        assertEquals(3,fractionOne.getDenominator());
+    }
+
+    @Test
+    void testMultiplyFractions() {
+        Fraction otherFraction = new Fraction(1,2);
+        otherFraction.multiply(fractionHalf);
+        fractionHalf.multiply(twoThird);
+        assertEquals(1,fractionHalf.getNumerator());
+        assertEquals(3,fractionHalf.getDenominator());
+        assertEquals(1,otherFraction.getNumerator());
+        assertEquals(4,otherFraction.getDenominator());
+    }
+
+    @Test
+    void testDivideFractions() {
+        Fraction otherFraction = new Fraction(1,4);
+        otherFraction.divide(fractionHalf);
+        fractionHalf.divide(twoThird);
+        assertEquals(1, otherFraction.getNumerator());
+        assertEquals(2, otherFraction.getDenominator());
+        assertEquals(3, fractionHalf.getNumerator());
+        assertEquals(4, fractionHalf.getDenominator());
+    }
 }
